4de1c171a6ca634027c9d29a64c395be71163585
Manuel Ulate <fr3zs09@gmail.com>
2023-01-19 16:52:00 -0600

    setup de material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import { CssBaseline, createTheme, ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app'
 
+const basicTheme = createTheme({
+  palette: {
+    mode: 'light'
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ basicTheme }>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
