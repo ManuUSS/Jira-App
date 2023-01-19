0744005392532ea3cfacdd0e511b7e86f2d2dc39
Manuel Ulate <fr3zs09@gmail.com>
2023-01-19 16:58:57 -0600

    creacion de temas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import '@/styles/globals.css'
 import { CssBaseline, createTheme, ThemeProvider } from '@mui/material';
+import { lightTheme, darkTheme } from 'themes';
 import type { AppProps } from 'next/app'
 
-const basicTheme = createTheme({
-  palette: {
-    mode: 'light'
-  }
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ basicTheme }>
+    <ThemeProvider theme={ lightTheme }>
       <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
