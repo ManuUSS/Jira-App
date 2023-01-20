3bea365c8cfbdbeb523b7263d7845c07c0a7be77
Manuel Ulate <fr3zs09@gmail.com>
2023-01-20 15:20:16 -0600

    post en server side

--- src/pages/api/entries/index.ts
+++ src/pages/api/entries/index.ts
@@ -4,6 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data =  
     | { message: string }
+    | IEntry[]
     | IEntry
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
@@ -12,6 +13,9 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
         case 'GET':
             return getEntries( res );
     
+        case 'POST':
+            return postEntry( req, res );
+
         default:
             return res.status(400).json({ message: 'EndPoint no existe' })
     }
@@ -28,5 +32,32 @@ const getEntries = async ( res: NextApiResponse<Data> ) => {
     await db.disconnect();
 
     res.status(200).json( entries )
+
+}
+
+const postEntry = async ( req: NextApiRequest , res:NextApiResponse<Data> ) => {
+
+    const { description = '' } = req.body();
+
+    const newEntry = new Entry({
+        description,
+        createdAt: Date.now(),
+    });
+
+    try {
+
+        await db.connect();
+        await newEntry.save();
+        await db.disconnect();
+        
+        return res.status(201).json( newEntry );
+
+    } catch ( error ) {
+        
+        await db.disconnect();
+        console.log( error );
+
+        return res.status(500).json({ message: 'Algo salió mal, revisa la consola del servidor' });
+    }
 
 }
