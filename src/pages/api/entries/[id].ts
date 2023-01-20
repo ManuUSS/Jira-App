1b881a7b4abf66c2a25acbcaea74c29940e068aa
Manuel Ulate <fr3zs09@gmail.com>
2023-01-20 15:57:46 -0600

    tener entrada por id

--- src/pages/api/entries/[id].ts
+++ src/pages/api/entries/[id].ts
@@ -16,7 +16,10 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
     switch ( req.method ) {
         case 'PUT':
             return updateEntry( req, res );
-    
+        
+        case 'GET':
+            return getEntryById( req, res );
+
         default:
             return res.status(400).json({ message: 'Método no existe' });
     }
@@ -52,6 +55,24 @@ const updateEntry = async ( req: NextApiRequest, res: NextApiResponse ) => {
         res.status(400).json({ message: error.errors.status.message.message });
     }
 
+
+}
+
+const getEntryById = async ( req: NextApiRequest, res: NextApiResponse ) => {
+
+    const { id } = req.query;
+
+    await db.connect();
+
+    const entryRequested = await Entry.findById( id );
+    await db.disconnect();
+
+    if( !entryRequested ){
+        await db.disconnect();
+        return res.status(400).json({ message: 'No hay entrada con ese ID - ' + id });
+    } 
+
+    res.status( 200 ).json( entryRequested );
 
 }
 
