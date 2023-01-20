8a49f34e41acd5806b38bd3de0a63ef36088253d
Manuel Ulate <fr3zs09@gmail.com>
2023-01-20 17:43:28 -0600

    middleware funcionando

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,17 @@ import type { NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
+    if( request.nextUrl.pathname.startsWith('/api/entries/') ) {
+        const id = request.nextUrl.pathname.replace('/api/entries/', '');
+        const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+        if( !checkMongoIDRegExp.test( id ) ) {
+            const url = request.nextUrl.clone();
+            url.pathname = '/api/bad-request';
+            url.search = `message?=${id} no es un IDMongo válido`;
+            return NextResponse.rewrite(url);  
+        }
+    }
+
     return NextResponse.next();
 
 }
