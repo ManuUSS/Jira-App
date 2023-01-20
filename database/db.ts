67e776229731df94f57345408bf981482739cfc2
Manuel Ulate <fr3zs09@gmail.com>
2023-01-20 14:29:30 -0600

    conectado a MongoDB

--- database/db.ts
+++ database/db.ts
@@ -15,7 +15,7 @@ const mongoConnection = {
 export const connect = async () => {
     
     if( mongoConnection.isConnected === 1 ){
-        console.log('Ya estamos conectados');
+        console.log('Ya estabamos conectados');
         return;
     }
 
@@ -30,13 +30,13 @@ export const connect = async () => {
         await disconnect();
     }
 
-    await mongoose.connect('')
+    await mongoose.connect( process.env.MONGO_URL || '' );
     mongoConnection.isConnected = 1;
-    console.log('Conectado a MongoDB', '....')
+    console.log('Conectado a MongoDB',  process.env.MONGO_URL );
 }
 
 export const disconnect = async () => {
-    if( mongoConnection.isConnected !== 0 ) return;
+    if( mongoConnection.isConnected === 0 ) return;
     await mongoose.disconnect();
     console.log('Desconectado de MongoDB')
 }
